ebf09b1500088e3f0bbc0cf29dc90daf3e0b701e
Luuklooijenga <luuklooijenga@gmail.com>
2015-03-23 12:57:03 +0100

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -15,13 +15,30 @@ def getLastWord(tweetlist):
     lastWords = [item.split()[-1] for item in tweetlist]
     lastWords = [''.join(c for c in s if c not in string.punctuation) for s in lastWords]
     print(lastWords)
-    
+
+def compare(lastWord, searchSound):
+    eList = []
+    for i in lastWord:
+        for o in searchSound:
+            if i == o:
+                eList.append(i)
+
+
+
+def searchWord(lastWords):
+    reList = []
+    with open('dpw.cd') as infile:
+        for line in infile:
+            line = line.strip().split('\\')
+            reList.append(line[1])
+    return reList      
+
 
 def main():
     x = getTweets()
-    getLastWord(x)
-    
-
+    lw = getLastWord(x)
+    sw = searchWord(lw)
+    c = compare(lw,sw)
 
 if __name__ == "__main__":
     main()
